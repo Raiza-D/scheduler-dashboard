b5fc72703cc5bfb2424cf31d834c9560cf5dc879
Raiza De Guzman <santos_dg7@hotmail.com>
2022-08-11 17:01:54 -0700

    Set initial value of loading to false

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,7 +5,7 @@ import Loading from "./Loading";
 
 class Dashboard extends Component {
   state = {
-    loading: true
+    loading: false
   }
 
   render() {
@@ -14,7 +14,7 @@ class Dashboard extends Component {
     if (this.state.loading) {
       return <Loading />;
     }
-    
+
     return <main className={dashboardClasses} />
   }
 }
